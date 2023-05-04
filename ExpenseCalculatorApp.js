a970ea90201fbc121665f79d2793e82af814559d
ahmar35 <muhammadahmar35@gmail.com>
2023-05-04 20:46:08 +0530

    ExpensePasswordBcrypt

--- ExpenseCalculatorApp.js
+++ ExpenseCalculatorApp.js
@@ -1,61 +1,62 @@
-const express=require('express')
-const path=require('path')
-const bodyParser=require('body-parser')
-const cors=require('cors')
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const sequelize = require('./UTIL/database')
-const UserDetails=require('./models/expenseCalculatorModels')
+const bcrypt = require('bcrypt')
+const UserDetails = require('./models/expenseCalculatorModels')
 const { error } = require('console')
-const app=express()
+const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
 
-app.post('/User/SignUp',async(req,res,next)=>{
-    try{
-    const Name=req.body.NAME
-    const Email=req.body.EMAIL
-    const Password=req.body.PASSWORD
-   
-    
-    
-   const info=await UserDetails.create({Name,Email,Password})
-        res.status(201).json(info)
-}catch(err){
-    res.status(403).json(err)
+app.post('/User/SignUp', (req, res, next) => {
 
-}
- 
-} )
+    const Name = req.body.NAME
+    const Email = req.body.EMAIL
+    const Password = req.body.PASSWORD
+    const saltRounds = 10;
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+        bcrypt.hash(Password, salt, function (err, hash) {
+            const info = UserDetails.create({ Name, Email, Password: hash }).then(() => {
+                res.status(201).json({ message: 'Successfuly create new user' })
+            }).catch(err => {
+                res.status(403).json(err);
+            })
 
-app.post('/User/LogIn',async(req,res,next)=>{
-    try{
-    
-    
-
-    const Email=req.body.EMAIL
-    const Password=req.body.PASSWORD
-    const User=await UserDetails.findAll()
-    for (i=0;i<User.length;i++){
-
-        if(User[i].Email===Email){
-            if(User[i].Password===Password){
-                res.status(201).json({message:'loginSuccesful'})
-            }else {
-                return res.status(401).json({message:'Incorrect Password'})
-            }
-        }
-    }
-    return res.status(404).json({message:'User Not Found'}) 
-
-}catch(err){
-
-
-}
-
+        })
+    })
 
 
 })
 
-sequelize.sync(/* {force:true} */)
+//userLogin
+app.post('/User/LogIn', async (req, res, next) => {
+    try {
+        const Email = req.body.EMAIL
+        const Password = req.body.PASSWORD
+        const User = await UserDetails.findAll({ where: { Email } })
 
+        if (User.length > 0) {
+            bcrypt.compare(Password, User[0].Password, (err, result) => {
+                if (err) {
+                    res.status(500).json({ message: 'Something Went Wrong' })
+                } else if (result == true) {
+                    res.status(201).json({ message: 'loginSuccesful' })
+                } else {
+                    return res.status(401).json({ message: 'Incorrect Password' })
+                }
+            })
+        }
+        else {
+            return res.status(404).json({ message: 'User Not Found' })
+        }
+    } catch (err) {
+        res.status(400).json({ message: 'Something Went Wrong' })
+
+    }
+})
+//user Login End
+sequelize.sync(/* {force:true} */)
 app.listen(3000)
