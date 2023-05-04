07d9414b296ac8fffc9c243a93f0f2157ebc1bf5
ahmar35 <muhammadahmar35@gmail.com>
2023-05-04 17:08:39 +0530

    ExpenseCalculatorLoginBackend

--- ExpenseCalculatorApp.js
+++ ExpenseCalculatorApp.js
@@ -3,7 +3,7 @@ const path=require('path')
 const bodyParser=require('body-parser')
 const cors=require('cors')
 const sequelize = require('./UTIL/database')
-const UserCredentials=require('./models/expenseCalculatorModels')
+const UserDetails=require('./models/expenseCalculatorModels')
 const { error } = require('console')
 const app=express()
 app.use(cors())
@@ -15,21 +15,46 @@ app.post('/User/SignUp',async(req,res,next)=>{
     const Name=req.body.NAME
     const Email=req.body.EMAIL
     const Password=req.body.PASSWORD
-    const info =await UserCredentials.findAll()
-    for (var i=0;i<info.length;i++){
-        if (info[i].Email===Email){
-            res.status(403).json(info)
-        }
-    }
+   
     
     
-   await UserCredentials.create({Name,Email,Password})
+   const info=await UserDetails.create({Name,Email,Password})
+        res.status(201).json(info)
 }catch(err){
-    console.log(err)
+    res.status(403).json(err)
 
 }
  
 } )
+
+app.post('/User/LogIn',async(req,res,next)=>{
+    try{
+    
+    
+
+    const Email=req.body.EMAIL
+    const Password=req.body.PASSWORD
+    const User=await UserDetails.findAll()
+    for (i=0;i<User.length;i++){
+
+        if(User[i].Email===Email){
+            if(User[i].Password===Password){
+                res.status(201).json({message:'loginSuccesful'})
+            }else {
+                return res.status(401).json({message:'Incorrect Password'})
+            }
+        }
+    }
+    return res.status(404).json({message:'User Not Found'}) 
+
+}catch(err){
+
+
+}
+
+
+
+})
 
 sequelize.sync(/* {force:true} */)
 
